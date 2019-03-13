670aaafc5ad1d84657913c2bdaf5a30e880a10c6
Lukas Stanek <ls@elostech.cz>
2019-03-13 08:22:43 +0100

    Cleanup pre projekty dev a prod

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 
-oc delete project cicd-tasks
+oc delete project cicd-tasks-dev
+oc delete project cicd-tasks-prod
 oc delete project cicd-components
 oc delete project cicd-jenkins
 
