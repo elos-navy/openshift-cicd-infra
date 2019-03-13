5d40f56c31775cd86d9d94b6b65721cb2b0014c0
Lukas Stanek <ls@elostech.cz>
2019-03-13 10:02:36 +0100

    Uprava jenkins pipelines do separatnych templates

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,12 @@
 
 ./cleanup.sh
 
-oc process -f templates/jenkins-clusterroles-template.yaml | oc create -f -
-oc process -f templates/jenkins-template.yaml | oc create -f -
+oc process -f templates/jenkins-project-template.yaml | oc create -f -
+oc project cicd-jenkins
+
+oc new-app jenkins-persistent --param VOLUME_CAPACITY=4Gi
+
+oc process -f templates/jenkins-infra-pipeline-template.yaml | oc create -f -
+oc process -f templates/jenkins-app-pipeline-template.yaml | oc create -f -
+
 oc start-build bc/infra-pipeline -n cicd-jenkins
