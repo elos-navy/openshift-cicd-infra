a9ba913250cc0a5683260c57e9f37eedf45258ce
Lukáš Staněk <ls@elostech.cz>
2019-05-13 16:08:00 +0200

    bootstrap.sh fix pre template

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,9 @@
 oc process -f templates/jenkins-project-template.yaml | oc create -f -
 oc project cicd-jenkins
 
-oc new-app jenkins-persistent --param VOLUME_CAPACITY=4Gi
+oc new-app \
+  --template=jenkins-persistent \
+  --param VOLUME_CAPACITY=4Gi
 
 oc process -f templates/jenkins-infra-pipeline-template.yaml | oc create -f -
 oc process -f templates/jenkins-app-pipeline-template.yaml | oc create -f -
